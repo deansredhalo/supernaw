fc4fa1384504591418c88ecb9a2b01cc6cf3411f
T-Jay Tipps <deansredhalo@gmail.com>
2015-03-10 23:52:53 -0500

    supernaw lives

--- app.js
+++ app.js
@@ -3,15 +3,95 @@ var protagonist = require('protagonist');
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-// var models = require('./models/model');
+var Schema = mongoose.Schema;
+var Model;
 var app = express();
+var router = express.Router();
 
 // declare our global variables
-var inputFile = 'apis/test.md';
+var inputFile = 'apis/test2.md';
 var responses = [];
+var responseModels = [];
+var createdSchemas = [];
 var fileData;
 
-function walkBlueprint(fileObject) {
+var Supernaw = function(file) {
+	this.readFile(file);
+}
+
+Supernaw.prototype.declareRoutes = function(items) {
+
+	for (var i = 0; i < items.length; i++) {
+		var item = items[i];
+
+		var tmpModel = module.exports.base.models[item.title].modelName;
+
+		if (item.title === tmpModel) {
+			var the_model = module.exports;
+
+			console.log(the_model);
+
+			if (item.method == 'GET') {
+				router.route(item.path).get(function(req, res) {
+
+					the_model.find(function(err, returns) {
+						if (err) {
+							return res.send(err);
+						}
+						res.json(returns);
+					});
+				});
+			}
+			else if (item.method == 'POST') {
+				router.route(item.path).post(function(req, res) {
+					var tmpItem = module.exports.base.models[item.title](req.body);
+
+					tmpItem.save(function(err) {
+						if (err) {
+							return res.send(err);
+						}
+					 
+						res.send({ message: 'Item Added' });
+					});
+				});
+			}
+		}
+	}
+}
+
+Supernaw.prototype.declareSchemas = function(models) {
+
+	var tmpObj = {};
+	var schemaObj = {};
+
+	for (var i = 0; i < models.length; i++) {
+		var model = models[i];
+
+		tmpObj['title'] = model.title;
+
+		if (model.method == 'POST') {
+
+			for (key in model.body) {
+				tmpObj[key] = model.body[key];
+			}
+		}
+
+		for (key in tmpObj) {
+			schemaObj[key] = (typeof tmpObj[key]);
+		}
+
+		var mongooseSchema = new Schema(schemaObj);
+
+		createdSchemas.push(mongooseSchema);
+
+		module.exports = mongoose.model(tmpObj['title'], mongooseSchema);
+	}
+
+	supernaw.declareRoutes(models);
+
+}
+
+Supernaw.prototype.walkBlueprint = function(fileObject) {
 
 	var resourceGroups = fileObject.resourceGroups;
 
@@ -34,22 +114,55 @@ function walkBlueprint(fileObject) {
 					var example = action['examples'][k];
 
 					responses.push( 
-						{
+						{	
+							"name": action.name,
 							"method": action.method,
 							"path": path,
 							"model": resource.model,
 							"responses": example.responses 
 						}
 					);
-
-					console.log(responses);
 				}
 			}
 		}
 	}
+
+	for (var x = 0; x < responses.length; x++) {
+		var response = responses[x];
+
+		response.name = response.name.replace(/(\r\n|\n|\r)/gm,"");
+		response.method = response.method.replace(/(\r\n|\n|\r)/gm,"");
+		response.path = response.path.replace(/(\r\n|\n|\r)/gm,"");
+
+		for (key in response.model) {
+			var item = response.model[key];
+			if (typeof item === 'string') {
+				item = item.replace(/(\r\n|\n|\r)/gm,"");
+			}
+		}
+
+		for (key in response.responses) {
+			var item = response.responses[key];
+			if (typeof item === 'string') {
+				item = item.replace(/(\r\n|\n|\r)/gm,"");
+			}
+		}
+
+		responseModels.push(
+			{
+				"title": response.name,
+				"body": JSON.parse(response.model.body),
+				"method": response.method,
+				"path": response.path
+			}
+		);
+
+	}
+
+	supernaw.declareSchemas(responseModels);
 }
 
-function parseFile(file) {
+Supernaw.prototype.parseFile = function(file) {
 
 	protagonist.parse(file, function(error, result) {
 		
@@ -59,27 +172,31 @@ function parseFile(file) {
 		}
 
 		resultJSON = result.ast;
-		walkBlueprint(resultJSON);
+		supernaw.walkBlueprint(resultJSON);
 
 	});
 
 }
 
-function readFile(file) {
+Supernaw.prototype.readFile = function(file) {
 	try {
 		var fileData = fs.readFileSync(file, 'utf8');
-		parseFile(fileData);
+		this.parseFile(fileData);
 	} 
 	catch (error) {
 		throw error;
 	}
 }
 
-readFile(inputFile);
-console.log(responses);
+var supernaw = new Supernaw(inputFile);
+
+var dbName = 'supernaw';
+var connectionString = 'mongodb://localhost:27017/' + dbName;
+ 
+mongoose.connect(connectionString);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-// app.use('/api', models);
+app.use('/', router);
 
 module.exports = app;
